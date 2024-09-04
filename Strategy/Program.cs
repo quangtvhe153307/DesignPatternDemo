e15db27c50a9779505542c41a458bdc99bc73632
quangtvhe153307 <qtran9587@gmail.com>
2024-09-04 23:42:39 +0700

    Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Context context = new Context();
+            IChoice choice;
+            choice = new FirstChoice();
+
+
+            context.SetChoice(choice);
+            context.ShowChoice();
+
+            choice = new SecondChoice();
+            context.SetChoice(choice);
+            context.ShowChoice();
+        }
+    }
+}
