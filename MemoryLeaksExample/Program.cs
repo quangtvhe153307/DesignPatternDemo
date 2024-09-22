e36121841052d7f88ebb87dca390e6cce451fab6
quangtvhe153307 <qtran9587@gmail.com>
2024-09-22 22:36:14 +0700

    MemoryLeaksExample

---
+++ MemoryLeaksExample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MemoryLeaksExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<int, SimpleEventClass> dic = new Dictionary<int, SimpleEventClass>();
+
+            for (int i = 0; i < 500000; i++)
+            {
+                using (dic[i] = new SimpleEventClass())
+                {
+                    //dic[i] = new SimpleEventClass() { Id = i};
+                    var ersult = dic[i].SimpleDelegate("Event raised");
+                    Console.WriteLine("Current object no.{0}", dic[i].Id + 1);
+                    dic[i] = null;
+                }
+
+            }
+        }
+    }
+}
---
+++ MemoryLeaksExample/SimpleEventClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryLeaksExample
+{
+    public class SimpleEventClass : IDisposable
+    {
+        public int Id { get; set; }
+        public MyDelegate SimpleDelegate;
+        public bool disposed = false;
+
+        public SimpleEventClass()
+        {
+            SimpleDelegate += new MyDelegate(PrintText);
+        }
+
+        public string PrintText(string text)
+        {
+            return text;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.SimpleDelegate != null)
+                {
+                    SimpleDelegate -= new MyDelegate(PrintText);
+                    Console.WriteLine("Unsubcribe printtext");
+                    disposed = true;
+                }
+            }
+        }
+
+        ~SimpleEventClass()
+        {
+            Dispose(false);
+        }
+    }
+}
