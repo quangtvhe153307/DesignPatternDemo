b5bdf7610a50ce5421f421bc942549b080468bc2
quangtvhe153307 <qtran9587@gmail.com>
2024-09-22 21:57:20 +0700

    GCExample

---
+++ GarbageCollectionExample/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionExample
+{
+    public class MyClass : IDisposable
+    {
+        private int MyInt;
+        private double MyDouble;
+
+        public MyClass()
+        {
+            MyInt = 25;
+            MyDouble = 100.5;
+        }
+        public void ShowMe()
+        {
+            Console.WriteLine("MyClass.ShowMe()");
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public void Dispose()
+        //{
+        //    GC.SuppressFinalize(this);
+        //    Console.WriteLine("Dispose is called");
+
+        //    Console.WriteLine("Total memory: " + GC.GetTotalMemory(false));
+        //}
+        ~MyClass()
+        {
+            Console.WriteLine("Destructor is called");
+            Console.WriteLine("After this destruction total memory: " + GC.GetTotalMemory(false));
+
+        }
+    }
+}
---
+++ GarbageCollectionExample/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace GarbageCollectionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                Console.WriteLine("Maximum Generations of GC: " + GC.MaxGeneration);
+                Console.WriteLine("Total Memory: " + GC.GetTotalMemory(false));
+
+                MyClass ob = new();
+                Console.WriteLine("ob is in generation: {0}", GC.GetGeneration(ob));
+                Console.WriteLine("Now, total memory is: {0}", GC.GetTotalMemory(false));
+
+                Console.WriteLine("Collection occurred in 0th generation: {0}", GC.CollectionCount(0));
+                Console.WriteLine("Collection occurred in 1st generation: {0}", GC.CollectionCount(1));
+                Console.WriteLine("Collection occurred in 2nd generation: {0}", GC.CollectionCount(2));
+
+                GC.Collect(0);
+                Console.WriteLine("\nAfter GC.Collection(0)");
+                Console.WriteLine("Collection occurred in 0th generation: {0}", GC.CollectionCount(0));
+                Console.WriteLine("Collection occurred in 1st generation: {0}", GC.CollectionCount(1));
+                Console.WriteLine("Collection occurred in 2nd generation: {0}", GC.CollectionCount(2));
+                Console.WriteLine("ob is in generation: {0}", GC.GetGeneration(ob));
+                Console.WriteLine("Now, total memory is: {0}", GC.GetTotalMemory(false));
+                
+                GC.Collect(1);
+                Console.WriteLine("\nAfter GC.Collection(0)");
+                Console.WriteLine("Collection occurred in 0th generation: {0}", GC.CollectionCount(0));
+                Console.WriteLine("Collection occurred in 1st generation: {0}", GC.CollectionCount(1));
+                Console.WriteLine("Collection occurred in 2nd generation: {0}", GC.CollectionCount(2));
+                Console.WriteLine("ob is in generation: {0}", GC.GetGeneration(ob));
+                Console.WriteLine("Now, total memory is: {0}", GC.GetTotalMemory(false));
+                
+                GC.Collect(2);
+                Console.WriteLine("\nAfter GC.Collection(0)");
+                Console.WriteLine("Collection occurred in 0th generation: {0}", GC.CollectionCount(0));
+                Console.WriteLine("Collection occurred in 1st generation: {0}", GC.CollectionCount(1));
+                Console.WriteLine("Collection occurred in 2nd generation: {0}", GC.CollectionCount(2));
+                Console.WriteLine("ob is in generation: {0}", GC.GetGeneration(ob));
+                Console.WriteLine("Now, total memory is: {0}", GC.GetTotalMemory(false));
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
